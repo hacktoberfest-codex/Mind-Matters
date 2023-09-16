72928cbde00015656cae01b4fb919b12b25d3c7f
Abh-I-sek <abhisek211004@gmail.com>
2023-09-17 00:48:07 +0530

    More work in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
+import Login from './components/authentication/login';
+import Signup from './components/authentication/signup';
 
 function App() {
 
@@ -10,6 +12,8 @@ function App() {
     <Router>
     <Routes>
       <Route path='/' element={<Landing/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
     </Routes>
     </Router>
   </div>  
